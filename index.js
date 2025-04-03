55bf3db912b521d4fbc9c6dc7efd3430cefe1231
Luz Arlet Gonzalez Claudio <2311080440@alumno.utpuebla.edu.mx>
2025-04-03 16:10:43 -0600

    vinculando a Cluster visualizado en MongoCompass

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// Importando librería CORS
+//CORS
 const cors = require('cors');
 
-// importando rutas de usuario
-const userRoute = require('./routes/user.route');
-// importando rutas de ubicaciones
-const locationRoute = require('./routes/location.route');
-// importando rutas de artículos
-const itemRoute = require('./routes/item.route');
+//Ruta del usuario.
+const userRoute = require('./routes/usuario.route');
+//Ruta de productos
+const prodRoute = require('./routes/producto.route');
+//Ruta del carrito de compras
+const cartRoute = require('./routes/carrito.route');
 
 const app = express();
-// Agregando el parser JSON de express
+// Parser del JSOn express.
 app.use(express.json());
 // Agregando el middleware de CORS para consumo de APIs en el mismo origen
 app.use(cors());
 
-// Ruta por defecto
+// Ruta por default.
 app.get('/', (req, res) => {
     res.send('Bienvenido al servidor de APIs version 1.0.0');
 })
@@ -27,16 +27,16 @@ app.get('/', (req, res) => {
 // U - update -> put
 // D - delete -> delete
 
-// Endpoints para colección de usuarios
+// Endpoint para colec de usuarios.
 app.use('/api/users', userRoute);
-// Endpoints para colección de ubicaciones
-app.use('/api/locations', locationRoute);
-// Endpoints para colección de artículos
-app.use('/api/items', itemRoute);
+// Endpoint para colec de productos
+app.use('/api/products', prodRoute);
+// Endpoints para colec de carrito de compras
+app.use('/api/cart', cartRoute);
 
 // Realizar petición de conexión a mongodb
 // mongoose.connect('mongodb://localhost:27017/MyDatabase')
-mongoose.connect('mongodb+srv://danielvazquez:uDxcNzeplxNqRBK5@cluster0.n7ayk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/myDatabase')
+mongoose.connect('mongodb+srv://arly:lawliet79@servecluster.vjjmn8c.mongodb.net/CastOxxo')
 .then( () => {
     console.log('Se estableció la conexión a base de datos exitosamente');
     app.listen( 3000, () => {
