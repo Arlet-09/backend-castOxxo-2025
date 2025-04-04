703b8d23e6f0288f04dc8d8120fb4ff5a475caa3
Luz Arlet Gonzalez Claudio <2311080440@alumno.utpuebla.edu.mx>
2025-04-03 18:08:02 -0600

    Backend listo para desplegar en Render

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
-//CORS
 const cors = require('cors');
-
-//Ruta del usuario.
-const userRoute = require('./routes/usuario.route');
-//Ruta de productos
-const prodRoute = require('./routes/producto.route');
-//Ruta del carrito de compras
-const cartRoute = require('./routes/carrito.route');
+require('dotenv').config(); // Cargar variables de entorno
 
 const app = express();
-// Parser del JSOn express.
+
+// Middlewares
 app.use(express.json());
-// Agregando el middleware de CORS para consumo de APIs en el mismo origen
 app.use(cors());
 
-// Ruta por default.
+// Importar rutas
+const userRoute = require('./routes/usuario.route');
+const prodRoute = require('./routes/producto.route');
+const cartRoute = require('./routes/carrito.route');
+
+// Rutas
 app.get('/', (req, res) => {
-    res.send('Bienvenido al servidor de APIs version 1.0.0');
-})
+    res.send('Bienvenido al servidor de APIs versión 1.0.0');
+});
 
-// Tareas CRUD y sus métodos
-// C - Create -> post
-// R - Recuperación -> get
-// U - update -> put
-// D - delete -> delete
-
-// Endpoint para colec de usuarios.
 app.use('/api/users', userRoute);
-// Endpoint para colec de productos
 app.use('/api/products', prodRoute);
-// Endpoints para colec de carrito de compras
 app.use('/api/cart', cartRoute);
 
-// Realizar petición de conexión a mongodb
-// mongoose.connect('mongodb://localhost:27017/MyDatabase')
-mongoose.connect('mongodb+srv://arly:lawliet79@servecluster.vjjmn8c.mongodb.net/CastOxxo')
-.then( () => {
-    console.log('Se estableció la conexión a base de datos exitosamente');
-    app.listen( 3000, () => {
-        console.log('Servidor trabajando en el puerto 3000');
-    });
+// Conexión a MongoDB Atlas
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-.catch( () => console.log('Ocurrió un error en la conexión a la base de datos') );
+.then(() => console.log('Conexión a MongoDB Atlas establecida'))
+.catch(err => console.error('Error al conectar a MongoDB:', err.message));
+
+// Puerto del servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor trabajando en el puerto ${PORT}`);
+});
